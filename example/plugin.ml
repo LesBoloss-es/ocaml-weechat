53d1ae7bfaa437c60aef2916cf8494efa8492a3c
Martin Pépin <kerl@wkerl.me>
2020-12-13 00:07:46 +0100

    In the example: add a /shrug command

--- example/plugin.ml
+++ example/plugin.ml
@@ -4,16 +4,29 @@ let say buffer _ argv_eol =
   let message = argv_eol.(1) in
   Weechat.command
     buffer
-    (Format.sprintf "From OCaml: \"%s\"\n" message)
+    (Format.sprintf "From OCaml: \"%s\"" message)
+
+let shrug buffer _ _ =
+  Weechat.command buffer "¯\\_(ツ)_/¯"
 
 let weechat_plugin_init () =
-  Weechat.hook_command
+  let _ = Weechat.hook_command
     "ocamlsay"
     "Say something from OCaml"
     "msg"
     "msg: the message to say"
     ""
     say
+  in
+  let _ = Weechat.hook_command
+    "shrug"
+    "print a shrug emoji in the current buffer"
+    ""
+    ""
+    ""
+    shrug
+  in
+  ()
 
 let () =
   Callback.register "weechat_plugin_init" weechat_plugin_init
