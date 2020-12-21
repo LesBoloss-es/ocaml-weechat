dd23f0489a20c03022037f7b35cee4d3b14aee6e
Martin Pépin <kerl@wkerl.me>
2020-12-22 00:03:30 +0100

    weechat_prefix always return a string

--- src/api/bindings_display.c
+++ src/api/bindings_display.c
@@ -9,13 +9,7 @@
 value caml_weechat_prefix(value prefix) {
   CAMLparam1(prefix);
   CAMLlocal1(res);
-  const char* p = weechat_prefix(String_val(prefix));
-  if (p) {
-    res = caml_alloc(1, 0);
-    Store_field(res, 0, caml_copy_string(p));
-  } else {
-    res = Val_int(0);
-  }
+  res = caml_copy_string(weechat_prefix(String_val(prefix)));
   CAMLreturn(res);
 }
 
